168e289306b619a2ea4cea414eb4b52b89c7c5ba
Ubuntu <ubuntu@ip-172-31-89-194.ec2.internal>
2019-09-20 18:21:07 +0000

    initial commit

---
+++ game.c
@@ -0,0 +1,15 @@
+// Copyright (c) 2019 Trinity Armstrong All rights reserved.
+//
+// Created by: Trinity Armstrong
+// Created on: September 2019
+// Hello World for Gameboy
+
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main {
+    printf("Hello, World!");
+
+    return 0;
+}
